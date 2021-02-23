03cc4c65c4d0c7cf2373f3f965c40465042cc310
cuvillier-ryan <cuvillier.ryan@gmail.com>
2021-02-23 12:15:00 -0600

    JavaUdemy

--- src/com/projectNext/helloworld/HelloWorld.java
+++ src/com/projectNext/helloworld/HelloWorld.java
@@ -1,7 +1,19 @@
 package com.projectNext.helloworld;
 
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, IntelliJ IDEA!");
+
+    public static void printYearsAndsDays(long minutes){
+        if(minutes < 0){
+            System.out.println("Invalid Value");
+        } else {
+            long hours = minutes / 60;
+//            long days = hours / 24;
+//            long years = days / 365;
+//            if(days >= 365) {
+//                days = 0;
+//            }
+//            System.out.println(minutes + " min = " + years + " y and " + days + " d");
+            System.out.println(hours);
+        }
     }
 }
---
+++ src/com/projectNext/helloworld/Main.java
@@ -0,0 +1,10 @@
+package com.projectNext.helloworld;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, IntelliJ IDEA!");
+
+        HelloWorld.printYearsAndsDays(345);
+    }
+}
+
