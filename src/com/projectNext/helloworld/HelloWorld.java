d2e1b461a1f469ff8361708a5ea0bf189ac19937
cuvillier-ryan <cuvillier.ryan@gmail.com>
2021-02-24 11:11:30 -0600

    PlayingCat

--- src/com/projectNext/helloworld/HelloWorld.java
+++ src/com/projectNext/helloworld/HelloWorld.java
@@ -26,4 +26,25 @@ public class HelloWorld {
             System.out.println("Neither all are equal or different");
         }
     }
+
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+        if (summer) {
+            if (temperature >= 25 && temperature <= 45) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        else {
+            if (temperature >= 25 && temperature <= 35) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+
+
 }
